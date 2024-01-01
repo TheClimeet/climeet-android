d120c16043de10af57dbf28a4e440042e99ed99a
박진성 <plashdof@gmail.com>
2024-01-02 04:24:42 +0900

    [CMA-32] feat : 네이버, 카카오 로그인 초기화 코드 구현

--- app/src/main/java/com/climus/climeet/app/App.kt
+++ app/src/main/java/com/climus/climeet/app/App.kt
@@ -3,6 +3,12 @@ package com.climus.climeet.app
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
+import com.climus.climeet.BuildConfig
+import com.climus.climeet.presentation.util.Constants.TAG
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -22,6 +28,18 @@ class App : Application(){
         super.onCreate()
         sharedPreferences =
             applicationContext.getSharedPreferences("APP", MODE_PRIVATE)
+        initSocialLogin()
+    }
+
+    private fun initSocialLogin(){
+        Log.d(TAG, "keyhash : ${Utility.getKeyHash(this)}")
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+        NaverIdLoginSDK.initialize(this,
+            BuildConfig.NAVER_CLIENT_ID,
+            BuildConfig.NAVER_CLIENT_SECRET,
+            BuildConfig.NAVER_CLIENT_NAME
+        )
     }
 
 
