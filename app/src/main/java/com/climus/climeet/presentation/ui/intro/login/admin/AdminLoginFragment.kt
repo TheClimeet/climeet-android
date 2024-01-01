05ed0450fba51f2b6802bc11e2d941e3cdc16106
박진성 <plashdof@gmail.com>
2024-01-02 03:42:40 +0900

    [CMA-30] feat : 클라이머 로그인, 관리자 로그인 Fragment 생성, Navigation 적용

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/login/admin/AdminLoginFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.login.admin
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentAdminLoginBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class AdminLoginFragment: BaseFragment<FragmentAdminLoginBinding>(R.layout.fragment_admin_login){
+}
---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/login/climer/ClimerLoginFragment.kt
@@ -0,0 +1,9 @@
+package com.climus.climeet.presentation.ui.intro.login.climer
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentClimerLoginBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class ClimerLoginFragment: BaseFragment<FragmentClimerLoginBinding>(R.layout.fragment_climer_login) {
+
+}
