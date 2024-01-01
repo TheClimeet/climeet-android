0e8510c7847c84e134402cccbb5f4c232a9ae014
박진성 <plashdof@gmail.com>
2024-01-02 02:17:39 +0900

    [CMA-43] feat : onBoardFragment IntroActivity 연결. intro nav 셋팅

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/onboard/OnBoardFragment.kt
@@ -0,0 +1,14 @@
+package com.climus.climeet.presentation.ui.intro.onboard
+
+import android.os.Bundle
+import android.view.View
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentOnboardBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class OnBoardFragment: BaseFragment<FragmentOnboardBinding>(R.layout.fragment_onboard) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
