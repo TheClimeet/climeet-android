adf8b1bf46a91295414b6a7d3e3e93d02b939431
박진성 <plashdof@gmail.com>
2024-01-02 01:59:06 +0900

    [CMA-29] feat : Splash Activity 생성, Activity 구조 Setting

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/IntroActivity.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro
+
+import com.climus.climeet.databinding.ActivityIntroBinding
+import com.climus.climeet.presentation.base.BaseActivity
+
+class IntroActivity: BaseActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
+
+}
--- app/src/main/java/com/climus/climeet/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/splash/SplashActivity.kt
@@ -1,4 +1,32 @@
 package com.climus.climeet.presentation.ui.splash
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.climus.climeet.app.App
+import com.climus.climeet.databinding.ActivitySplashBinding
+import com.climus.climeet.presentation.base.BaseActivity
+import com.climus.climeet.presentation.ui.intro.IntroActivity
+import com.climus.climeet.presentation.ui.main.MainActivity
+import com.climus.climeet.presentation.util.Constants.X_ACCESS_TOKEN
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val jwt: String? = App.sharedPreferences.getString(X_ACCESS_TOKEN, null)
+            jwt?.let {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } ?: run {
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }
+        }, 2000)
+    }
 }
