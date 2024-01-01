8140d261b369b3a93b14041aabc4a45bd8930d6f
박진성 <plashdof@gmail.com>
2024-01-02 03:13:54 +0900

    [CMA-43] feat : 온보딩 마지막 페이지 Button 활성화 추가

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/onboard/OnBoardFragment.kt
@@ -2,6 +2,7 @@ package com.climus.climeet.presentation.ui.intro.onboard
 
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentOnboardBinding
 import com.climus.climeet.presentation.base.BaseFragment
@@ -11,8 +12,31 @@ class OnBoardFragment : BaseFragment<FragmentOnboardBinding>(R.layout.fragment_o
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setViewpager()
+    }
+
+    private fun setViewpager() {
         binding.vpOnboard.adapter =
             OnBoardAdapter(listOf("흩어져 있는 클라이밍 루트 정보", "어려웠던 클라이밍 운동 기록", "혼클할 때 서러웠던 루트 파인딩"))
         binding.idcOnboard.setViewPager(binding.vpOnboard)
+
+        binding.vpOnboard.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (binding.vpOnboard.currentItem == binding.vpOnboard.adapter?.itemCount?.minus(1)) {
+                    binding.btnStart.visibility = View.VISIBLE
+                } else {
+                    binding.btnStart.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageSelected(position: Int) {}
+        })
+
     }
 }
