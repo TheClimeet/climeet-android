776db7718e8a000a88c214f0d6e3c2a38144807b
박진성 <plashdof@gmail.com>
2024-01-02 05:03:36 +0900

    [CMA-32] feat : 관리자 회원가입, 클라이머 회원가입 첫단계 fragment 생성

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/climer/SetClimerNickFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.signup.climer
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentSetClimerNickBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class SetClimerNickFragment: BaseFragment<FragmentSetClimerNickBinding>(R.layout.fragment_set_climer_nick) {
+}
