7ebb8cfa5a490f8fa1c2e0a246f2692864ad2924
박진성 <plashdof@gmail.com>
2024-01-02 03:01:22 +0900

    [CMA-43] feat : 온보딩 ViewPager, indicator 연결

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/onboard/OnBoardFragment.kt
@@ -6,9 +6,13 @@ import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentOnboardBinding
 import com.climus.climeet.presentation.base.BaseFragment
 
-class OnBoardFragment: BaseFragment<FragmentOnboardBinding>(R.layout.fragment_onboard) {
+class OnBoardFragment : BaseFragment<FragmentOnboardBinding>(R.layout.fragment_onboard) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.vpOnboard.adapter =
+            OnBoardAdapter(listOf("흩어져 있는 클라이밍 루트 정보", "어려웠던 클라이밍 운동 기록", "혼클할 때 서러웠던 루트 파인딩"))
+        binding.idcOnboard.setViewPager(binding.vpOnboard)
     }
 }
