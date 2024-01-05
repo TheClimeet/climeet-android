1cee6b666c99a5eb907478310dd4a9dabbeaf1ac
seon <sjkim5621@naver.com>
2024-01-05 19:18:01 +0900

    [CMA-50] feat: id/pw 설정 화면 기능 완성
    아이디 비밀번호가 유효하면 다음 버튼이 활성화되고, 누르면 개인정보 입력으로 이동
    임시로 이전 화면은 SetCragNameFragment와 연결했습니다

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/SetCragNameFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/SetCragNameFragment.kt
@@ -1,8 +1,41 @@
 package com.climus.climeet.presentation.ui.intro.signup.admin
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragNameBinding
 import com.climus.climeet.presentation.base.BaseFragment
 
+
 class SetCragNameFragment: BaseFragment<FragmentSetCragNameBinding>(R.layout.fragment_set_crag_name) {
+
+    private val viewModel: SetCragNameViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 데이터 바인딩 변수에 뷰모델 연결
+        binding.vm = viewModel
+
+        initEventObserve()
+    }
+
+    private fun initEventObserve() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                when (it) {
+                    is SetCragNameEvent.NavigateToBack -> findNavController().navigateUp()  // 배경화면 설정으로 이동
+                    is SetCragNameEvent.NavigateToNext -> navigateNext()
+                }
+            }
+        }
+    }
+
+    // 알림 설정으로 이동
+    private fun navigateNext(){
+        val action = SetCragNameFragmentDirections.actionSetCragNameFragmentToSetAdminIdPwFragment2()
+        findNavController().navigate(action)
+    }
 }
---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/idpw/SetAdminIdPwViewModel.kt
@@ -0,0 +1,113 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.idpw
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.climus.climeet.presentation.ui.intro.signup.admin.AdminSignupForm
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+sealed class SetAdminIdPwEvent{
+    data object NavigateToBack : SetAdminIdPwEvent()
+    data object NavigateToNext : SetAdminIdPwEvent()
+}
+
+@HiltViewModel
+class SetAdminIdPwViewModel @Inject constructor() : ViewModel() {
+
+    private val _event = MutableSharedFlow<SetAdminIdPwEvent>()
+    val event: SharedFlow<SetAdminIdPwEvent> = _event.asSharedFlow()
+
+    val warningTextId = MutableStateFlow("")
+    val warningTextPw = MutableStateFlow("")
+
+    val id = MutableStateFlow("")
+    val pw = MutableStateFlow("")
+    val isNextButtonEnabled = MutableStateFlow(false)
+
+    // 버튼 상태 변화
+    fun updateNextButtonState() {
+        Log.d("admin", "button valid : " + (isIdValid(id.value) && isPasswordValid(pw.value)))
+        isNextButtonEnabled.value = isIdValid(id.value) && isPasswordValid(pw.value)
+    }
+
+    // 아이디 유효성 검사
+    private fun isIdValid(id: String): Boolean {
+        //todo : API 호출해 아이디 중복 처리 (동작 확인차 임시로 값 넣음)
+        return id != "test"
+    }
+
+    // 비밀번호 유효성 검사
+    private fun isPasswordValid(pw: String): Boolean {
+        val passwordPattern = Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}\$")
+        return passwordPattern.matches(pw)
+    }
+
+    // id, pw가 변경될 때 경고 텍스트를 초기화
+    init {
+        idObserve()
+        pwObserve()
+    }
+
+    private fun idObserve() {
+        id.onEach {
+            if (id.value.isBlank()) {
+                warningTextId.value = ""
+            } else {
+                if (!isIdValid(id.value)) {
+                    warningTextId.value = "중복된 아이디 입니다."
+                } else{
+                    warningTextId.value = ""
+                }
+            }
+            updateNextButtonState()
+        }.launchIn(viewModelScope)
+    }
+
+    private fun pwObserve() {
+        pw.onEach {
+            if (pw.value.isBlank()) {
+                warningTextPw.value = ""
+            } else {
+                if (!isPasswordValid(pw.value)) {
+                    warningTextPw.value = "영문과 숫자 조합 8자리 이상 입력하세요."
+                } else {
+                    warningTextPw.value = ""
+                }
+            }
+            updateNextButtonState()
+        }.launchIn(viewModelScope)
+    }
+
+    // 사업자 등록증 설정으로 이동
+    fun navigateToBack() {
+        viewModelScope.launch {
+            _event.emit(SetAdminIdPwEvent.NavigateToBack)
+        }
+    }
+
+    // 개인정보 설정으로 이동
+    fun navigateToNext(){
+        val nowId = id.value
+        val nowPw = pw.value
+
+        if (nowId.isNotBlank() && nowPw.isNotBlank() && isIdValid(nowId) && isPasswordValid(nowPw)) {
+            // 아이디 비번 저장
+            AdminSignupForm.setId(nowId)
+            AdminSignupForm.setPw(nowPw)
+
+            Log.d("admin", "아이디 비번 저장 : " + AdminSignupForm.id + ", " + AdminSignupForm.pw)
+            // 화면 전환
+            viewModelScope.launch {
+                _event.emit(SetAdminIdPwEvent.NavigateToNext)
+            }
+        }
+    }
+}
