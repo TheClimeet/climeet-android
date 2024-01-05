d333898a253d6ae94d8ab9456210ffa86f75bd9d
박진성 <plashdof@gmail.com>
2024-01-06 06:16:45 +0900

    [CMA-46] feat : 암장 검색 키워드 delete 구현

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/searchname/SearchCragNameViewModel.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/searchname/SearchCragNameViewModel.kt
@@ -35,4 +35,8 @@ class SearchCragNameViewModel @Inject constructor() : ViewModel() {
 
         }.launchIn(viewModelScope)
     }
+    
+    fun deleteKeyword(){
+        keyword.value = ""
+    }
 }
