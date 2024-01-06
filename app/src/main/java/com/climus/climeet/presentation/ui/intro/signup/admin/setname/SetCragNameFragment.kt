732c84ac98b91c6be868afe2450f8c4433d4bc3a
박진성 <plashdof@gmail.com>
2024-01-06 17:29:02 +0900

    [CMA-47] feat : 암장 선택 페이지 화면이동 및 event 구현

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/setname/SetCragNameFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/setname/SetCragNameFragment.kt
@@ -7,23 +7,23 @@ import android.text.SpannableString
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragNameBinding
 import com.climus.climeet.presentation.base.BaseFragment
-import com.climus.climeet.presentation.util.Constants.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SetCragNameFragment: BaseFragment<FragmentSetCragNameBinding>(R.layout.fragment_set_crag_name) {
+class SetCragNameFragment :
+    BaseFragment<FragmentSetCragNameBinding>(R.layout.fragment_set_crag_name) {
 
     private val viewModel: SetCragNameViewModel by viewModels()
     private val args: SetCragNameFragmentArgs by navArgs()
-    private val cragId by lazy{ args.cragId }
-
+    private val cragId by lazy { args.cragId }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -32,20 +32,22 @@ class SetCragNameFragment: BaseFragment<FragmentSetCragNameBinding>(R.layout.fra
         binding.vm = viewModel
         viewModel.setCragId(cragId)
         setWarningText()
+        initEventObserve()
     }
 
-    private val clickableSpan = object: ClickableSpan(){
+    private val clickableSpan = object : ClickableSpan() {
         override fun updateDrawState(tp: TextPaint) {
             tp.color = requireContext().getColor(R.color.cm_red)
             tp.typeface = Typeface.DEFAULT_BOLD
             tp.isUnderlineText = false
         }
+
         override fun onClick(widget: View) {
-            Log.d(TAG,"click")
+            findNavController().toSetCragNameError()
         }
     }
 
-    private fun setWarningText(){
+    private fun setWarningText() {
         val text = getString(R.string.set_crag_name_warning_text)
         val spannable = SpannableString(text)
             .apply {
@@ -59,6 +61,27 @@ class SetCragNameFragment: BaseFragment<FragmentSetCragNameBinding>(R.layout.fra
 
         binding.tvWarning.text = spannable
         binding.tvWarning.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun initEventObserve() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                when (it) {
+                    is SetCragNameEvent.NavigateToBusinessRegistration -> findNavController().toBusinessRegistration()
+                    is SetCragNameEvent.NavigateToBack -> findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    private fun NavController.toBusinessRegistration() {
+        val action = SetCragNameFragmentDirections.actionSetCragNameFragmentToBusinessRegistrationFragment()
+        navigate(action)
+    }
+
+    private fun NavController.toSetCragNameError() {
+        val action = SetCragNameFragmentDirections.actionSetCragNameFragmentToSetCragErrorFragment()
+        navigate(action)
     }
 
 }
--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/setname/SetCragNameViewModel.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/setname/SetCragNameViewModel.kt
@@ -1,29 +1,42 @@
 package com.climus.climeet.presentation.ui.intro.signup.admin.setname
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.climus.climeet.presentation.ui.intro.signup.admin.model.CragInfoUiData
 import com.climus.climeet.presentation.util.Constants.TEST_IMG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed class SetCragNameEvent {
+    data object NavigateToBack : SetCragNameEvent()
+    data object NavigateToBusinessRegistration : SetCragNameEvent()
+}
+
 @HiltViewModel
-class SetCragNameViewModel @Inject constructor(): ViewModel(){
+class SetCragNameViewModel @Inject constructor() : ViewModel() {
 
     private val _uiCragInfo = MutableStateFlow(CragInfoUiData())
     val uiCragInfo: StateFlow<CragInfoUiData> = _uiCragInfo.asStateFlow()
 
-    private var cragId: Long ?= null
+    private val _event = MutableSharedFlow<SetCragNameEvent>()
+    val event: SharedFlow<SetCragNameEvent> = _event.asSharedFlow()
 
-    fun setCragId(id: Long){
+    private var cragId: Long? = null
+
+    fun setCragId(id: Long) {
         cragId = id
         getCragInfo()
     }
 
-    private fun getCragInfo(){
+    private fun getCragInfo() {
         // todo 서버통신
 
         _uiCragInfo.update { state ->
@@ -32,6 +45,18 @@ class SetCragNameViewModel @Inject constructor(): ViewModel(){
                 name = "클라이머스 클라이밍",
                 state = false
             )
+        }
+    }
+
+    fun navigateToBack() {
+        viewModelScope.launch {
+            _event.emit(SetCragNameEvent.NavigateToBack)
+        }
+    }
+
+    fun navigateToBusinessRegistration() {
+        viewModelScope.launch {
+            _event.emit(SetCragNameEvent.NavigateToBusinessRegistration)
         }
     }
 
