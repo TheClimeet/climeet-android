d8c1f0b9b2867a7b329bb3f7871e46a6e1f997aa
박진성 <plashdof@gmail.com>
2024-01-06 17:20:31 +0900

    [CMA-49] feat : 암장 등록 오류 제출 완료 Fragment 생성

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorCompleteFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.error
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentSetCragErrorCompleteBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class SetCragErrorCompleteFragment: BaseFragment<FragmentSetCragErrorCompleteBinding>(R.layout.fragment_set_crag_error_complete) {
+}
