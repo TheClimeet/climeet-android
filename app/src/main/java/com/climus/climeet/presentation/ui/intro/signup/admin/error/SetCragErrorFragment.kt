3c034e468229bc06741c5ad4beafacd4732ef821
박진성 <plashdof@gmail.com>
2024-01-06 17:13:14 +0900

    [CMA-48] feat : 암장 등록 오류 Fragment 생성

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.error
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentSetCragErrorBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class SetCragErrorFragment: BaseFragment<FragmentSetCragErrorBinding>(R.layout.fragment_set_crag_error) {
+}
