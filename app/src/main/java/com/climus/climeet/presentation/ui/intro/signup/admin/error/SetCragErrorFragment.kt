900d003af30fb7c5d3d93f4784d2ba4a353e1fb2
박진성 <plashdof@gmail.com>
2024-01-07 01:39:23 +0900

    [CMA-48] feat : 암장 등록 오류 폼제출 event 정의, info data StateFlow 정의

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorFragment.kt
@@ -3,6 +3,8 @@ package com.climus.climeet.presentation.ui.intro.signup.admin.error
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragErrorBinding
@@ -10,7 +12,8 @@ import com.climus.climeet.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SetCragErrorFragment: BaseFragment<FragmentSetCragErrorBinding>(R.layout.fragment_set_crag_error) {
+class SetCragErrorFragment :
+    BaseFragment<FragmentSetCragErrorBinding>(R.layout.fragment_set_crag_error) {
 
     private val viewModel: SetCragErrorViewModel by viewModels()
     private val args: SetCragErrorFragmentArgs by navArgs()
@@ -23,6 +26,23 @@ class SetCragErrorFragment: BaseFragment<FragmentSetCragErrorBinding>(R.layout.f
 
         binding.vm = viewModel
         viewModel.setCragInfo(cragId, cragImgUrl, cragName)
+        initEventObserve()
+    }
+
+    private fun initEventObserve() {
+        repeatOnStarted {
+            viewModel.event.collect {
+                when (it) {
+                    is SetCragErrorEvent.NavigateToBack -> findNavController().navigateUp()
+                    is SetCragErrorEvent.NavigateToErrorComplete -> findNavController().toSetCragErrorComplete()
+                }
+            }
+        }
+    }
+
+    private fun NavController.toSetCragErrorComplete() {
+        val action = SetCragErrorFragmentDirections.actionSetCragErrorFragmentToSetCragErrorCompleteFragment()
+        navigate(action)
     }
 
 }
--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorViewModel.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorViewModel.kt
@@ -3,15 +3,30 @@ package com.climus.climeet.presentation.ui.intro.signup.admin.error
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed class SetCragErrorEvent {
+    data object NavigateToErrorComplete : SetCragErrorEvent()
+    data object NavigateToBack : SetCragErrorEvent()
+}
 
 @HiltViewModel
 class SetCragErrorViewModel @Inject constructor() : ViewModel() {
+
+    private val _event = MutableSharedFlow<SetCragErrorEvent>()
+    val event: SharedFlow<SetCragErrorEvent> = _event.asSharedFlow()
+
+    private val cragId = MutableStateFlow(-1L)
+    val cragImgUrl = MutableStateFlow("")
+    val cragName = MutableStateFlow("")
 
     val name = MutableStateFlow("")
     val phone = MutableStateFlow("")
@@ -36,7 +51,24 @@ class SetCragErrorViewModel @Inject constructor() : ViewModel() {
         id: Long,
         imgUrl: String,
         name: String
-    ){
+    ) {
+        cragId.value = id
+        cragImgUrl.value = imgUrl
+        cragName.value = name
+    }
 
+    fun submitErrorForm() {
+        viewModelScope.launch {
+
+            // todo 오류 form API 통신
+
+            _event.emit(SetCragErrorEvent.NavigateToErrorComplete)
+        }
+    }
+
+    fun navigateToBack() {
+        viewModelScope.launch {
+            _event.emit(SetCragErrorEvent.NavigateToBack)
+        }
     }
 }
