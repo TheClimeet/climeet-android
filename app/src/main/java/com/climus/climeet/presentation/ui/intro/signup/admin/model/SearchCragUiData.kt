a787641c970e273bfd04285d6d55c76b166505d3
박진성 <plashdof@gmail.com>
2024-01-06 15:37:10 +0900

    [CMA-46] style : SearchCragUiData 변수명 변경

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/model/SearchCragUiData.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/model/SearchCragUiData.kt
@@ -2,7 +2,7 @@ package com.climus.climeet.presentation.ui.intro.signup.admin.model
 
 data class SearchCragUiData(
     val id: Long,
-    val img: String = "",
+    val imgUrl: String = "",
     val name: String = "",
     val keyword: String = "",
     val onClickListener: (Long) -> Unit
