ed6ae9970ef8a85bf70afc71cc6e743273eadbf9
박진성 <plashdof@gmail.com>
2024-01-07 01:41:01 +0900

    [CMA-48] feat : 암장 등록 오류 폼제출 완료 Button ClickListener 정의

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorCompleteFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorCompleteFragment.kt
@@ -1,8 +1,27 @@
 package com.climus.climeet.presentation.ui.intro.signup.admin.error
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragErrorCompleteBinding
 import com.climus.climeet.presentation.base.BaseFragment
 
 class SetCragErrorCompleteFragment: BaseFragment<FragmentSetCragErrorCompleteBinding>(R.layout.fragment_set_crag_error_complete) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setBtnListener()
+    }
+
+    private fun setBtnListener(){
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.btnMain.setOnClickListener {
+            // todo 메인으로 돌아가기
+        }
+    }
 }
