466919a32d7e951eb901420c4dce2122f5d0fc6e
seon <sjkim5621@naver.com>
2024-01-07 00:17:54 +0900

    [CMA-53] feat: 서비스 설정 기능 구현
    리사이클러뷰 버튼을 이용해 서비스를 고를 수 있게함
    AdminSignupForm에 서비스 List가 들어감
    -> Service : id, title(서비스명), isSelected(포함 여부)

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/service/Service.kt
@@ -0,0 +1,7 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.service
+
+data class Service(
+    val id: Int = 0,
+    val title: String = "",
+    var isSelected: Boolean = false
+)
