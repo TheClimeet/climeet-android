cca090ce53d51cd320297244269b80d25be937cf
박진성 <plashdof@gmail.com>
2024-01-06 17:19:47 +0900

    [CMA-49] feat : 사업자등록증 제출 fragment 생성

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/submitdoc/BusinessRegistrationFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.submitdoc
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentBusinessRegistrationBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class BusinessRegistrationFragment: BaseFragment<FragmentBusinessRegistrationBinding>(R.layout.fragment_business_registration) {
+}
