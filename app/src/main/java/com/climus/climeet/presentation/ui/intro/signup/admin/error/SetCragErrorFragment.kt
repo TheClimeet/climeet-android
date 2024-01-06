56481435677e379acc1bd970859d504b9d6a39fe
박진성 <plashdof@gmail.com>
2024-01-07 00:38:14 +0900

    [CMA-48] feat : 암장 등록 오류 Fragment, ViewModel 생성

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorFragment.kt
@@ -1,8 +1,24 @@
 package com.climus.climeet.presentation.ui.intro.signup.admin.error
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragErrorBinding
 import com.climus.climeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SetCragErrorFragment: BaseFragment<FragmentSetCragErrorBinding>(R.layout.fragment_set_crag_error) {
+
+    private val args: SetCragErrorFragmentArgs by navArgs()
+    private val cragId by lazy { args.cragId }
+    private val cragImgUrl by lazy { args.cragImgUrl }
+    private val cragName by lazy { args.cragName }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 }
---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorViewModel.kt
@@ -0,0 +1,13 @@
+package com.climus.climeet.presentation.ui.intro.signup.admin.error
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SetCragErrorViewModel @Inject constructor(): ViewModel() {
+
+
+
+}
