efdc166c544584714216b87f4353b9620ef0ac07
박진성 <plashdof@gmail.com>
2024-01-07 18:27:35 +0900

    [CMA-48] feat : 암장 등록 오류 폼제출 완료 메인으로 이동 구현

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorCompleteFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/error/SetCragErrorCompleteFragment.kt
@@ -2,12 +2,14 @@ package com.climus.climeet.presentation.ui.intro.signup.admin.error
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetCragErrorCompleteBinding
 import com.climus.climeet.presentation.base.BaseFragment
 
-class SetCragErrorCompleteFragment: BaseFragment<FragmentSetCragErrorCompleteBinding>(R.layout.fragment_set_crag_error_complete) {
+class SetCragErrorCompleteFragment :
+    BaseFragment<FragmentSetCragErrorCompleteBinding>(R.layout.fragment_set_crag_error_complete) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,13 +17,19 @@ class SetCragErrorCompleteFragment: BaseFragment<FragmentSetCragErrorCompleteBin
         setBtnListener()
     }
 
-    private fun setBtnListener(){
+    private fun setBtnListener() {
         binding.btnBack.setOnClickListener {
             findNavController().navigateUp()
         }
 
         binding.btnMain.setOnClickListener {
-            // todo 메인으로 돌아가기
+            findNavController().toLogin()
         }
+    }
+
+    private fun NavController.toLogin() {
+        val action =
+            SetCragErrorCompleteFragmentDirections.actionSetCragErrorCompleteFragmentToLoginFragment()
+        navigate(action)
     }
 }
