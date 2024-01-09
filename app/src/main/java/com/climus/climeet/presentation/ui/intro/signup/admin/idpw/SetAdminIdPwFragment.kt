679bfddd08294367e284ef0d0d0649346edefa27
SeonJeong <sjkim5621@naver.com>
2024-01-09 19:37:47 +0900

    [CMA-50] feat: id/pw 정규식 패턴 만족에 따른 글자색, 밑줄색 변경

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/idpw/SetAdminIdPwFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/signup/admin/idpw/SetAdminIdPwFragment.kt
@@ -2,14 +2,15 @@ package com.climus.climeet.presentation.ui.intro.signup.admin.idpw
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentSetAdminIdPwBinding
 import com.climus.climeet.presentation.base.BaseFragment
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 class SetAdminIdPwFragment : BaseFragment<FragmentSetAdminIdPwBinding>(R.layout.fragment_set_admin_id_pw) {
 
@@ -21,6 +22,9 @@ class SetAdminIdPwFragment : BaseFragment<FragmentSetAdminIdPwBinding>(R.layout.
         // 데이터 바인딩 변수에 뷰모델 연결
         binding.vm = viewModel
 
+        observeColorChange(viewModel.idViewColor, binding.viewBar)
+        observeColorChange(viewModel.pwViewColor, binding.viewBar2)
+
         initEventObserve()
     }
 
@@ -31,6 +35,15 @@ class SetAdminIdPwFragment : BaseFragment<FragmentSetAdminIdPwBinding>(R.layout.
                     is SetAdminIdPwEvent.NavigateToBack -> findNavController().navigateUp() // 사업자 등록증 설정으로 이동
                     is SetAdminIdPwEvent.NavigateToNext -> navigateNext()
                 }
+            }
+        }
+    }
+
+    // color를 관찰하고, 변경될 때마다 EditText 아래 바 색상 업데이트
+    private fun observeColorChange(flow: Flow<Int>, view: View) {
+        lifecycleScope.launch {
+            flow.collect { color ->
+                view.setBackgroundColor(ContextCompat.getColor(requireContext(), color))
             }
         }
     }
