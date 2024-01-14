3a326508139f0c08102bf0a8a4e7ae5d03ea37ed
박진성 <plashdof@gmail.com>
2024-01-14 13:29:53 +0900

    [CMA-44] feat : ID/PW 찾기 Fragment 생성, navigation 적용

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/login/admin/FindIdPwCompleteFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.intro.login.admin
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentFindIdPwCompleteBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class FindIdPwCompleteFragment: BaseFragment<FragmentFindIdPwCompleteBinding>(R.layout.fragment_find_id_pw_complete) {
+}
