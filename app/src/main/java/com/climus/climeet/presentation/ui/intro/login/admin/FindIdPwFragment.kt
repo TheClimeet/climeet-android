4bb210edb73483614ff4af0bb7caa2d40ae62ae0
박진성 <plashdof@gmail.com>
2024-01-14 13:45:56 +0900

    [CMA-44] feat : ID/PW 찾기 Layout 구현, 화면이동 Setting

--- app/src/main/java/com/climus/climeet/presentation/ui/intro/login/admin/FindIdPwFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/intro/login/admin/FindIdPwFragment.kt
@@ -1,8 +1,25 @@
 package com.climus.climeet.presentation.ui.intro.login.admin
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentFindIdPwBinding
 import com.climus.climeet.presentation.base.BaseFragment
 
 class FindIdPwFragment: BaseFragment<FragmentFindIdPwBinding>(R.layout.fragment_find_id_pw) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSubmit.setOnClickListener {
+            findNavController().toFindIdPwComplete()
+        }
+    }
+
+    private fun NavController.toFindIdPwComplete(){
+        val action = FindIdPwFragmentDirections.actionFindIdPwFragmentToFindIdPwCompleteFragment()
+        navigate(action)
+    }
 }
