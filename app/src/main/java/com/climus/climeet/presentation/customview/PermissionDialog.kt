37b6e7c95d346871c9812012a0d225e3733e91ec
박진성 <plashdof@gmail.com>
2024-01-17 00:20:31 +0900

    [CMA-41] fix : 알람설정 Dialog 문구 수정

--- app/src/main/java/com/climus/climeet/presentation/customview/PermissionDialog.kt
+++ app/src/main/java/com/climus/climeet/presentation/customview/PermissionDialog.kt
@@ -39,6 +39,6 @@ class PermissionDialog(
 }
 
 enum class Permission(val code: Int, val msg: String) {
-    ALARM(Constants.ALARM_PERMISSION, "클밋의 다음 작업을 허용하시겠습니까?\n알림 수신"),
+    ALARM(Constants.ALARM_PERMISSION, "`클밋`의 알림을 허용해주세요!\n\n해당 기기에서 운동 완료 알림 등\n 서비스 이용에 필요한 안내 사항을\n푸시 알림으로 보내드릴게요.\n\n앱 푸시에 동의하시겠어요?"),
     STORAGE(Constants.STORAGE_PERMISSION, "클밋의 다음 작업을 허용하시겠습니까?\n기기, 사진, 미디어, 파일 엑세스")
 }
