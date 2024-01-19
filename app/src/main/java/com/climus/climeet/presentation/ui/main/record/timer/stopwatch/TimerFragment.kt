eb15f9d541b2a63ba1d2c0ce79aa30dd2e39be6c
SeonJeong <sjkim5621@naver.com>
2024-01-20 03:05:59 +0900

    [CMA-63] refactor: 스톱워치 버튼 이벤트 재설정
    viewMode 함수로 버튼 보이기 설정 정리
    정지 버튼 누르는 시간 별 기능 구현

--- app/src/main/java/com/climus/climeet/presentation/ui/main/record/timer/stopwatch/TimerFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/record/timer/stopwatch/TimerFragment.kt
@@ -2,6 +2,7 @@ package com.climus.climeet.presentation.ui.main.record.timer.stopwatch
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentRecordTimerBinding
@@ -21,30 +22,58 @@ class TimerFragment: BaseFragment<FragmentRecordTimerBinding>(R.layout.fragment_
 
         binding.vm = viewModel
 
-        // 시작 버튼 눌림 -> [gone: 시작 버튼] [현재 visible: 정지, 일시정지 버튼]
+        // 시작 버튼 눌림
         binding.ivStart.setOnClickListener {
             onStartClickListener?.onStartClick()    // Indicator 보이기 설정
 
-            it.visibility = View.GONE
-            binding.ivStop.visibility = View.VISIBLE
-            binding.ivPause.visibility = View.VISIBLE
+            viewMode("start")
         }
-        // 일시정지 버튼 눌림 -> [gone: 일시정지 버튼] [현재 visible: 정지, 재시작 버튼]
+        // 일시정지 버튼 눌림
         binding.ivPause.setOnClickListener {
-            it.visibility = View.GONE
-            binding.ivRestart.visibility = View.VISIBLE
+            viewMode("pause")
         }
-        // 재시작 버튼 눌림 -> [gone: 재시작 버튼] [현재 visible: 정지, 일시정지 버튼]
+        // 재시작 버튼 눌림
         binding.ivRestart.setOnClickListener {
-            it.visibility = View.GONE
-            binding.ivPause.visibility = View.VISIBLE
+            viewMode("restart")
         }
-        // 정지 버튼 눌림 -> [gone: 정지, 재시작, 일시정지 버튼] [현재 visible: 시작 버튼]
+        // 정지 버튼 눌림
         binding.ivStop.setOnClickListener {
-            it.visibility = View.GONE
-            binding.ivPause.visibility = View.GONE
-            binding.ivRestart.visibility = View.GONE
-            binding.ivStart.visibility = View.VISIBLE
+            // todo : 다이얼로그 띄우기
+            Toast.makeText(requireContext(), "길게 누르자", Toast.LENGTH_SHORT).show()
+        }
+        binding.ivStop.setOnLongClickListener {
+            viewMode("stop")
+            onStartClickListener?.onStartClick()    // Indicator 보이기 설정
+            true    // OnClickListener와 같이 쓰기 위해 true로 설정
+        }
+    }
+
+    private fun viewMode (mode : String){
+        when (mode) {
+            "start" -> {
+                // [gone: 시작 버튼] [현재 visible: 정지, 일시정지 버튼]
+                binding.ivStart.visibility = View.GONE
+                binding.ivStop.visibility = View.VISIBLE
+                binding.ivPause.visibility = View.VISIBLE
+            }
+            "pause" -> {
+                // [gone: 일시정지 버튼] [현재 visible: 정지, 재시작 버튼]
+                binding.ivPause.visibility = View.GONE
+                binding.ivRestart.visibility = View.VISIBLE
+            }
+            "restart" -> {
+                // [gone: 재시작 버튼] [현재 visible: 정지, 일시정지 버튼]
+                binding.ivRestart.visibility = View.GONE
+                binding.ivPause.visibility = View.VISIBLE
+
+            }
+            else -> { // "stop"
+                //[gone: 정지, 재시작, 일시정지 버튼] [현재 visible: 시작 버튼]
+                binding.ivStop.visibility = View.GONE
+                binding.ivPause.visibility = View.GONE
+                binding.ivRestart.visibility = View.GONE
+                binding.ivStart.visibility = View.VISIBLE
+            }
         }
     }
 }
