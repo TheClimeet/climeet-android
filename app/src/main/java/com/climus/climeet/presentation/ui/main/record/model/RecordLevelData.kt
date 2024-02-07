9405af6217411b0a3ded8dac07347df01757d2a7
SeonJeong <sjkim5621@naver.com>
2024-02-08 03:48:37 +0900

    [CMA-60] docs : 파일 삭제

--- app/src/main/java/com/climus/climeet/presentation/ui/main/record/model/RecordLevelData.kt
+++
@@ -1,8 +0,0 @@
-package com.climus.climeet.presentation.ui.main.record.model
-
-data class RecordLevelData(
-    val levelName: String = "",
-    val levelColor: String = "",
-    var isSelected: Boolean = false,
-    val onClickListener: (String) -> Unit
-)
