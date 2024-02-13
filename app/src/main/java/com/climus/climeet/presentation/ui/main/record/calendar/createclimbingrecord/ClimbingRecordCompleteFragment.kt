6f0e3cd23574789acd5776fb494eda0153c5d7c1
Inwoo <lys91892@gmail.com>
2024-02-14 00:02:47 +0900

    [CMA-58] fix : calendar 기록 완료시 동기화 문제 해결

--- app/src/main/java/com/climus/climeet/presentation/ui/main/record/calendar/createclimbingrecord/ClimbingRecordCompleteFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/record/calendar/createclimbingrecord/ClimbingRecordCompleteFragment.kt
@@ -5,15 +5,19 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.climus.climeet.R
 import com.climus.climeet.databinding.FragmentClimbingRecordCompleteBinding
 import com.climus.climeet.presentation.base.BaseFragment
+import com.climus.climeet.presentation.ui.main.record.calendar.CalendarViewModel
+import com.climus.climeet.presentation.ui.main.record.model.CreateRecordData
 
 
 class ClimbingRecordCompleteFragment :
     BaseFragment<FragmentClimbingRecordCompleteBinding>(R.layout.fragment_climbing_record_complete) {
+    private val viewModel: CalendarViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,6 +26,9 @@ class ClimbingRecordCompleteFragment :
         val drawable = binding.ivCheck.drawable
         if (drawable is Animatable) {
             (drawable as Animatable).start()
+        }
+        if (viewModel.selectedDate.value == CreateRecordData.selectedDate) {
+            viewModel.setRecord(CreateRecordData.selectedDate)
         }
 
         Handler(Looper.getMainLooper()).postDelayed({
