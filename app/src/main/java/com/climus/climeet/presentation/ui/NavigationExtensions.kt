2fe5d4faa2d801803ed7b5d9b2b749412842187b
박진성 <plashdof@gmail.com>
2024-02-16 01:00:17 +0900

    [CMA-41] feat : 유저프로필, 암장프로필 보기 Fragment 생성

--- app/src/main/java/com/climus/climeet/presentation/ui/NavigationExtensions.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/NavigationExtensions.kt
@@ -8,3 +8,14 @@ fun NavController.toShortsPlayer(shortsId: Long, position: Int) {
     val action = MainNavDirections.globalActionToShortsPlayerFragment(shortsId, position)
     navigate(action)
 }
+
+fun NavController.toGymProfile(){
+    val action = MainNavDirections.globalActionToGymProfileFragment()
+    navigate(action)
+}
+
+fun NavController.toClimerProfile(){
+    val action = MainNavDirections.globalActionToClimerProfileFragment()
+    navigate(action)
+}
+
