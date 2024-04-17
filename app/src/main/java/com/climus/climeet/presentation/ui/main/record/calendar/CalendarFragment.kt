76a0cf0dfa95f4724fdbf3841a1350053b7f16c4
Inwoo <lys91892@gmail.com>
2024-04-17 19:16:05 +0900

    [CMA-95] refactor : 날짜 설정 livedate -> stateflow

--- app/src/main/java/com/climus/climeet/presentation/ui/main/record/calendar/CalendarFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/record/calendar/CalendarFragment.kt
@@ -44,14 +44,8 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment
         setRecycler()
         initEventObserve()
         initStateObserve()
+        initDateObserve()
         customCalendar()
-
-        viewModel.selectedDate.observe(viewLifecycleOwner, Observer { date ->
-            val yearMonth = YearMonth.of(date.year, date.monthValue)
-            binding.calendarView.dayBinder = null
-            setupDayBinder()
-            binding.calendarView.scrollToMonth(yearMonth)  // 새로 선택된 날짜가 있는 월로 이동
-        })
 
     }
 
@@ -73,6 +67,18 @@ class CalendarFragment : BaseFragment<FragmentCalendarBinding>(R.layout.fragment
             }
         }
     }
+
+    private fun initDateObserve(){
+        repeatOnStarted {
+            viewModel.selectedDate.collect { date ->
+                val yearMonth = YearMonth.of(date.year, date.monthValue)
+                binding.calendarView.dayBinder = null
+                setupDayBinder()
+                binding.calendarView.scrollToMonth(yearMonth)  // 새로 선택된 날짜가 있는 월로 이동
+            }
+        }
+    }
+
 
     private fun initEventObserve() {
         repeatOnStarted {
