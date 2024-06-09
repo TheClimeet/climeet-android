1fb95a6ab0de71efc2a95eb059905b38a9f70f3a
SeonJeong <sjkim5621@naver.com>
2024-06-09 19:15:17 +0900

    [CMA-139] feat : 유저 프로필 현재 공개 범위에 해당하는 버튼 색 설정
    - 현재 상태에 해당하는 버튼 색상을 main으로 설정한다

--- app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/climer/viewpager/MyPageClimberProfileShortsViewModel.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/climer/viewpager/MyPageClimberProfileShortsViewModel.kt
@@ -2,7 +2,6 @@ package com.climus.climeet.presentation.ui.main.mypage.myprofile.climer.viewpage
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.climus.climeet.presentation.ui.main.mypage.myprofile.climer.model.MyPageClimberProfileUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,13 +13,18 @@ data class ClimberProfileShortsBtnState(
     val shorts: Boolean = false,
 )
 
+data class ClimberShortsVisibilityState (
+    val public: Boolean = true,
+    val follow: Boolean = false,
+    val closed: Boolean = false,
+    val state: String = "전체공개"
+)
+
 @HiltViewModel
 class MyPageClimberProfileShortsViewModel @Inject constructor() : ViewModel() {
-    // todo : 공개 범위 버튼 설정 가져오기 및 변경 사항 서버에 반영
 
-    // 선택한 공개 범위 설정
-    private val _uiState = MutableStateFlow(MyPageClimberProfileUiState())
-    val uiState: StateFlow<MyPageClimberProfileUiState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(ClimberShortsVisibilityState())
+    val uiState: StateFlow<ClimberShortsVisibilityState> = _uiState.asStateFlow()
 
     // 공개 범위 설정 버튼 관리
     private val _btnState = MutableStateFlow(ClimberProfileShortsBtnState())
@@ -36,20 +40,31 @@ class MyPageClimberProfileShortsViewModel @Inject constructor() : ViewModel() {
     }
 
     /*
-     * [전체보기] [나만보기] 버튼 중 하나를 눌렀을 때 해당 버튼으로 공개 범위를 설정
-     * 두 선택지 버튼이 다시 안 보이게 설정
-     */
-    fun setShortsState(visibility: Boolean) {
+    * [전체보기] [팔로우만] [나만보기] 버튼 중 하나를 눌렀을 때 해당 버튼으로 공개 범위를 설정
+    * 선택지 버튼이 다시 안 보이게 설정
+    */
+    fun setShortsVisibility(target: Int) {
         _uiState.update { state ->
-            state.copy(
-                shortsPublic = visibility
-            )
+            when (target) {
+                1 -> {
+                    state.copy(public = true, follow = false, closed = false, state = "전체공개")
+                }
+                2 -> {
+                    state.copy(public = false, follow = true, closed = false, state = "팔로우만")
+                }
+                3 -> {
+                    state.copy(public = false, follow = false, closed = true, state = "나만보기")
+                }
+                else -> {
+                    state.copy(public = true, follow = false, closed = false, state = "전체공개")
+                }
+            }
         }
+
         _btnState.update { state ->
             state.copy(
                 shorts = false
             )
         }
-        Log.d("mypage_edit", "숏츠 : $visibility")
     }
 }
