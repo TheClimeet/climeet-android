d0056842aa67b79d3b9ab873d9fd50c63719d6f3
박진성 <plashdof@gmail.com>
2024-06-12 00:09:10 +0900

    [CMA-41] version : 0.2.7 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.climus.climeet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 16
-        versionName = "0.2.6"
+        versionCode = 17
+        versionName = "0.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_DEV_URL", getProperty("baseDevUrl"))
