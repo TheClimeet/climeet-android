8b398200ae4612b8c169107b06f9a57626ce4441
박진성 <plashdof@gmail.com>
2024-06-12 05:10:14 +0900

    [CMA-147] feat : 사진 확대, 축소, 이동 기능 구현

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/admin/setroute/MyPageAdminSetRouteFragment.kt
@@ -0,0 +1,120 @@
+package com.climus.climeet.presentation.ui.main.mypage.myprofile.admin.setroute
+
+import android.annotation.SuppressLint
+import android.graphics.Matrix
+import android.graphics.PointF
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.View
+import android.widget.ImageView
+import androidx.fragment.app.viewModels
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentMypageAdminSetrouteBinding
+import com.climus.climeet.presentation.base.BaseFragment
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+class MyPageAdminSetRouteFragment :
+    BaseFragment<FragmentMypageAdminSetrouteBinding>(R.layout.fragment_mypage_admin_setroute) {
+
+    private val viewModel: MyPageAdminSetRouteViewModel by viewModels()
+
+    internal enum class TouchMode {
+        NONE,
+        ONE_FINGER,
+        TWO_FINGER
+    }
+
+    private var touchMode: TouchMode? = null
+    private var matrix: Matrix? = null
+    private var savedMatrix: Matrix? = null
+    private var startPoint: PointF? = null
+    private var midPoint: PointF? = null
+    private var oldDistance = 0f
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        matrix = Matrix()
+        savedMatrix = Matrix()
+        binding.ivRoute.setOnTouchListener(onTouch)
+        binding.ivRoute.scaleType = ImageView.ScaleType.MATRIX
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private val onTouch = View.OnTouchListener { v, event ->
+        if (v == binding.ivRoute) {
+            val action = event.action
+            when (action and MotionEvent.ACTION_MASK) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    touchMode = TouchMode.ONE_FINGER
+                    downSingleEvent(event)
+                }
+
+                MotionEvent.ACTION_POINTER_DOWN -> if (event.pointerCount == 2) {
+                    touchMode = TouchMode.TWO_FINGER
+                    downMultiEvent(event)
+                }
+
+                MotionEvent.ACTION_MOVE -> if (touchMode === TouchMode.ONE_FINGER) {
+                    moveSingleEvent(event)
+                } else if (touchMode === TouchMode.TWO_FINGER) {
+                    moveMultiEvent(event)
+                }
+
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> touchMode = TouchMode.NONE
+            }
+        }
+        true
+    }
+
+    private fun downSingleEvent(event: MotionEvent) {
+        savedMatrix?.set(matrix)
+        startPoint = PointF(event.x, event.y)
+    }
+
+    private fun downMultiEvent(event: MotionEvent) {
+        oldDistance = getDistance(event)
+        if (oldDistance > 5f) {
+            savedMatrix?.set(matrix)
+            midPoint = getMidPoint(event)
+        }
+    }
+
+    private fun moveSingleEvent(event: MotionEvent) {
+        matrix?.set(savedMatrix)
+        matrix?.set(savedMatrix)
+        matrix?.postTranslate(event.x - startPoint!!.x, event.y - startPoint!!.y)
+        binding.ivRoute.imageMatrix = matrix
+    }
+
+    private fun moveMultiEvent(event: MotionEvent) {
+        val newDistance = getDistance(event)
+        if (newDistance > 5f) {
+            matrix?.set(savedMatrix)
+            val scale = newDistance / oldDistance
+            matrix?.postScale(scale, scale, midPoint!!.x, midPoint!!.y)
+            matrix?.postRotate(0.0.toFloat(), midPoint!!.x, midPoint!!.y)
+            binding.ivRoute.imageMatrix = matrix
+        }
+    }
+
+    private fun getMidPoint(e: MotionEvent): PointF {
+        val x = (e.getX(0) + e.getX(1)) / 2
+        val y = (e.getY(0) + e.getY(1)) / 2
+        return PointF(x, y)
+    }
+
+    private fun getDistance(e: MotionEvent): Float {
+        val x = e.getX(0) - e.getX(1)
+        val y = e.getY(0) - e.getY(1)
+        return sqrt((x * x + y * y).toDouble()).toFloat()
+    }
+
+
+
+}
---
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/admin/setroute/MyPageAdminSetRouteViewModel.kt
@@ -0,0 +1,9 @@
+package com.climus.climeet.presentation.ui.main.mypage.myprofile.admin.setroute
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageAdminSetRouteViewModel @Inject constructor(): ViewModel() {
+}
