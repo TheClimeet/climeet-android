8d38869aee3b0244ec2cdf6e5f315c99a55c2ad6
SeonJeong <sjkim5621@naver.com>
2024-06-19 01:27:46 +0900

    [CMA-156] feat : 리뷰 작성 페이지 링크 이동 설정
    - 임시 링크 삽입

--- app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/review/MyPageReviewFragment.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/review/MyPageReviewFragment.kt
@@ -1,5 +1,7 @@
 package com.climus.climeet.presentation.ui.main.mypage.review
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,5 +14,22 @@ import com.climus.climeet.presentation.base.BaseFragment
 
 class MyPageReviewFragment : BaseFragment<FragmentMyPageReviewBinding>(R.layout.fragment_my_page_review) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        initClickListener()
+    }
+
+    private fun initClickListener(){
+        // todo : 구글 플레이스토어 연결
+        binding.btnReview.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/games?hl=ko-KR"))
+            startActivity(intent)
+        }
+        // todo : 토스 익명 계좌 연결
+        binding.btnSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://toss.im/"))
+            startActivity(intent)
+        }
+    }
 }
