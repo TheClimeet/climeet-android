1ca496eeb39cc41d51873fd28774f8b5539a057e
SeonJeong <sjkim5621@naver.com>
2024-06-21 15:15:53 +0900

    [CMA-157] feat: 암장 프로필 사진, 닉네임 편집 ui 구현

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/admin/editprofile/MyPageAdminProfileEditBackgroundFragment.kt
@@ -0,0 +1,9 @@
+package com.climus.climeet.presentation.ui.main.mypage.myprofile.admin.editprofile
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentMypageAdminProfileBackgroundEditBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class MyPageAdminProfileEditBackgroundFragment :
+    BaseFragment<FragmentMypageAdminProfileBackgroundEditBinding>(R.layout.fragment_mypage_admin_profile_background_edit) {
+}
---
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/admin/editprofile/MyPageAdminProfileEditFragment.kt
@@ -0,0 +1,12 @@
+package com.climus.climeet.presentation.ui.main.mypage.myprofile.admin.editprofile
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentMypageAdminProfileEditBinding
+import com.climus.climeet.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MyPageAdminProfileEditFragment :
+    BaseFragment<FragmentMypageAdminProfileEditBinding>(R.layout.fragment_mypage_admin_profile_edit) {
+
+}
