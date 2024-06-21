e15b67f0d8bbd566e8aa7ed7b30d5027b48c4ffc
SeonJeong <sjkim5621@naver.com>
2024-06-21 15:16:55 +0900

    [CMA-158] feat: 암장 서비스 수정 페이지 ui 구현

---
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/myprofile/admin/info/MyPageAdminProfileEditServiceFragment.kt
@@ -0,0 +1,8 @@
+package com.climus.climeet.presentation.ui.main.mypage.myprofile.admin.info
+
+import com.climus.climeet.R
+import com.climus.climeet.databinding.FragmentMypageAdminProfileEditServiceBinding
+import com.climus.climeet.presentation.base.BaseFragment
+
+class MyPageAdminProfileEditServiceFragment: BaseFragment<FragmentMypageAdminProfileEditServiceBinding>(R.layout.fragment_mypage_admin_profile_edit_service) {
+}
