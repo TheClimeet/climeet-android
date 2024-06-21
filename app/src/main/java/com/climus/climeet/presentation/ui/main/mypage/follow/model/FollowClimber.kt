0128bf29305b67585ff7d4f31b493dfcd9692a87
SeonJeong <sjkim5621@naver.com>
2024-06-21 17:21:20 +0900

    [CMA-156] refactor : 운영정책 링크 삽입

--- app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/follow/model/FollowClimber.kt
+++ app/src/main/java/com/climus/climeet/presentation/ui/main/mypage/follow/model/FollowClimber.kt
@@ -1,13 +1,10 @@
-package com.climus.climeet.presentation.ui.main.mypage.follow
-
+package com.climus.climeet.presentation.ui.main.mypage.follow.model
 
 data class FollowClimber(
-
     val userId: Long,
     val climberName: String = "",
     val profileImageUrl : String = "",
     val keyword: String = "",
     var followerCount : Long = 0,
     var isFollowing: Boolean = false
-
 )
